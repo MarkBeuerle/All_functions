ebbf9155977250a961e9b3895c16987bfa0ca607
MarkBeuerle <43422557+MarkBeuerle@users.noreply.github.com>
2018-11-02 17:47:04 +0100

    Add files via upload

---
+++ Basisconcepten/Getallen, Strings and Conversion.py
@@ -0,0 +1,7 @@
+cijferICOR= 7
+cijferPROG=7
+cijferCSN= 7
+gemiddelde= (cijferCSN+cijferICOR+cijferPROG)/3
+beloning=(cijferPROG*30)+(cijferICOR*30)+(cijferCSN*30)
+
+print('Mijn cijfers (gemiddeld een'+str(gemiddelde)+ ') leveren een beloning van '+str(beloning)+ ' euro op!')
---
+++ Basisconcepten/Input_output.py
@@ -0,0 +1,4 @@
+uurloon = int(input("Wat verdien je per uur : "))
+uren = int(input("Hoeveel uur werk je : "))
+salaris =(uurloon*uren)
+print(str(uren)+" uur werken levert "+str(salaris)+ " Euro op.")
